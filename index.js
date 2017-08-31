2946ed77c91aaa7439f8e6a75994f8068f5b5f29
dperrymorrow <dperrymorrow@gmail.com>
2017-08-31 14:18:28 -0700

    setting up the read current dir

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
 "use strict";
 
 const fs = require("fs");
+const path = require("path");
+const dir = process.cwd();
+const colors = require("colors");
+
+fs.readdir(dir, (err, files) => {
+  if (err) {
+    throw err;
+  }
+
+  files
+    .map(file => {
+      return path.join(p, file);
+    })
+    .filter(file => {
+      return fs.statSync(file).isFile();
+    })
+    .forEach(file => {
+      console.log("%s (%s)", file, path.extname(file));
+    });
+});
