473e8b241b7dd829e0b72b100974943a3d9591d4
dperrymorrow <dperrymorrow@gmail.com>
2017-09-06 13:16:01 -0700

    option for deleting original files

--- index.js
+++ index.js
@@ -1,5 +1,27 @@
 #! /usr/bin/env node
 
+require("colors");
+const inquirer = require("inquirer");
 const traverse = require("./lib/traverse");
+const parser = require("./lib/parser");
+const fs = require("fs");
 
-traverse.parseDir(process.cwd(), 0, false);
+inquirer
+  .prompt([
+    {
+      type: "confirm",
+      name: "nuke",
+      message: "Delete all js files with template: defined and index.styl files?",
+    },
+  ])
+  .then(answers => {
+    traverse.parseDir(process.cwd(), 0, false);
+    if (answers.nuke) {
+      console.log("deleting original files".yellow);
+      setTimeout(() => {
+        parser.deleteQue.forEach(fs.unlinkSync);
+        console.log("finished deletion".green);
+      }, 2000);
+    }
+  })
+  .catch(console.log);
